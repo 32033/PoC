146be8c8d0b4fe9e2610b3de0b5b6be18a7a2047
GreenJon902 <greenjon902@gmail.com>
2024-03-27 21:36:53 +0000

    pawn movement case 1 and 2

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from piece_checkers import *
 window = tk.Tk()
 board = Board()
 
-currently_selected: Union[tuple[int, int], None] = [1, 0]
+currently_selected: Union[tuple[int, int], None] = [1, 1]
 
 
 def update_buttons():
@@ -17,22 +17,29 @@ def update_buttons():
         for y in range(board.size):
             piece_type = board.get(x, y)
 
-            disabled = (piece_type == "") or ((currently_selected != None) and (not check_if_possible_move([x, y])))
+            enabled: bool
+            if currently_selected is None:
+                enabled = piece_type != ""
+            else:
+                enabled = check_if_possible_move([x, y])
 
-            b = tk.Button(window, text=piece_type, state=tk.DISABLED if disabled else tk.NORMAL)
+            piece_type = piece_type if piece_type != "" else "."
+
+            b = tk.Button(window, text=piece_type, state=tk.DISABLED if not enabled else tk.NORMAL)
             b.grid(row=y, column=x)
 
 """
     Checks if it is possible to move the piece at currently_selected to the coords at coords.
-    Returns true if they are the same coords.
+    Returns true if the move is possible.
+    Returns true if the move is the same.
 """
-def check_if_possible_move(coords):
-    piece_color, piece_type = board.get(*coords)
+def check_if_possible_move(destination):
+    piece_color, piece_type = board.get(*currently_selected)
 
-    if coords == currently_selected:  # Same piece return true
+    if destination == currently_selected:  # Same piece return true
         return True
-    elif check_pawn(board, piece_color, currently_selected, coords):
-        pass
+    elif piece_type == "p" and check_pawn(board, piece_color, currently_selected, destination):
+        return True
     else:
         return False
 
--- piece_checkers.py
+++ piece_checkers.py
@@ -1,11 +1,24 @@
-import maths
+import math
+
 
 def check_pawn(board, color, from_, to):
-    # Check move is valid move
-    if color == "b":
-        if not (from_[1] + 1 == to[1] and ):
-            return false
-    #math.abs(from_[0] - to[0]) == 1
-    if from_[0] != to[0]:
-        return true
-    return False
+    """
+    Checks if a given move is possible for a pawn, there are 4 cases for this.
+    Move one forward: when spot in front is free.
+    Move two forward: when both spots in front are free, and only on first turn (y is 1 for black, or 6 for white).
+    Move one diagonally forward: when a piece is diagonally in front of it.
+    Move one diagonally forward: When the piece directly to that side is a pawn that has just moved two forward.
+    """
+
+    # Case 1 and 2
+    if from_[0] == to[0]:
+        direction = 1 if color == "b" else -1
+        first_turn = from_[1] == ((8 + direction) % 8)
+        if (from_[1] + direction == to[1]) and (board.get(*to) == ""):
+            return True
+        elif first_turn and (from_[1] + direction * 2 == to[1]) and (board.get(to[0], to[1] + direction) == "") and (board.get(*to) == ""):
+            return True
+        else:
+            return False
+    else:
+        return False  # TODO: Rest of pawn movement
